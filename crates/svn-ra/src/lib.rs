038a188e52590fe204b7a474ad03e9068141aa6b
Yiyu Lin <linyiyu1992@gmail.com>
2025-06-19 23:04:20 +0800

    feat(ra): porting session

--- crates/svn-ra/src/lib.rs
+++ crates/svn-ra/src/lib.rs
@@ -1,6 +1,7 @@
 mod capability;
 mod loader;
 mod ra_layer;
+pub mod session;
 mod svn;
 
 pub use capability::RaCapability;
--- crates/svn-ra/src/session.rs
+++ crates/svn-ra/src/session.rs
@@ -1,7 +1,57 @@
 //! session
 
+/* The RA session object. */
 /// A repository access session.  This object is used to perform requests
 /// to a repository, identified by a URL.
 ///
 /// `svn_ra_session_t`
-pub struct SvnRaSession {}
+///
+/// `libsvn_ra_svn/client.c`
+pub struct SvnRaSession; //(Box<dyn RaSession>);
+
+impl SvnRaSession {}
+
+pub type SvnVersion = String;
+
+/// `svn_ra__vtable_t`
+pub trait RaSession {
+    /// This field should always remain first in the vtable.
+    fn get_version(&self) -> SvnVersion;
+
+    /// Return a short description of the RA implementation, as a localized
+    /// string.
+    fn get_description(&self) -> &'static str;
+    /// Return a list of actual URI schemes supported by this implementation.
+    /// The returned array is NULL-terminated.
+    fn get_schemes(&self) -> Vec<&'static str> {
+        vec!["svn"]
+    }
+    /* See svn_ra_open5(). */
+    // All fields in SESSION, except priv, have been initialized by the
+    // time this is called.  SESSION->priv may be set by this function.
+    fn open_session(corrected_url: &str, redirect_url: &str, session_url: &str) -> Self
+    where
+        Self: Sized;
+}
+/// Implementation of the `RaSession` trait for `SvnRaSession`.
+impl RaSession for SvnRaSession {
+    fn get_version(&self) -> SvnVersion {
+        "1.0.0".to_string() // Placeholder version
+    }
+
+    fn get_description(&self) -> &'static str {
+        if cfg!(feature = "_sasl") {
+            r#"Module for accessing a repository using the svn network protocol.
+              - with Cyrus SASL authentication"#
+        } else {
+            r#"Module for accessing a repository using the svn network protocol."#
+        }
+    }
+
+    fn open_session(corrected_url: &str, redirect_url: &str, session_url: &str) -> Self
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
+}
